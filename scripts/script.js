e7f1bf828ee9ecba9721bdff6dc743fd3c547e2c
mrYegor <mr.yegor@yahoo.com>
2021-10-11 21:11:42 +0300

    conflict

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 console.log('Hello, friends!')
+
+
+window.onload = function() {
+    var btn = document.querySelector('button');
+    
+    btn.onclick = function() {
+        console.log('Maybe conflict happened')
+    }
+}
